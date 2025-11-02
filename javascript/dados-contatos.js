de90d7566957ed2d117425590410b0bc386ba3c9
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2025-11-02 01:44:34 -0300

    atualização e-mail contato
    grupopesquisamind@gmail.com

--- javascript/dados-contatos.js
+++ javascript/dados-contatos.js
@@ -8,7 +8,7 @@ function listarItens(chave) {
       nome: "Ana Paula Kawabe de Lima Ferreira",
       funcao: "Profa. EBTT de Química",
       tipoParticipacao: "Coordenadora",
-      email: "equipemindifspjcr@gmail.com",
+      email: "grupopesquisamind@gmail.com",
       lattes: "http://lattes.cnpq.br/4564160743198905",
       imagem: "ana_kawabe.jpg"  // <-- Só o nome do arquivo
     },
@@ -18,7 +18,7 @@ function listarItens(chave) {
       nome: "Alexssandro Ferreira da Silva",
       funcao: "Téc. de Tecnologia da Informação",
       tipoParticipacao: "Colaborador Interno",
-      email: "equipemindifspjcr@gmail.com",
+      email: "grupopesquisamind@gmail.com",
       lattes: "http://lattes.cnpq.br/3843853231671192",
       imagem: "alexssandro.jpg"  // <-- Só o nome do arquivo
 
@@ -29,7 +29,7 @@ function listarItens(chave) {
       nome: "Tardelli Ronan Coelho Stekel",
       funcao: "Prof. EBTT de Informática",
       tipoParticipacao: "Colaborador Interno",
-      email: "equipemindifspjcr@gmail.com",
+      email: "grupopesquisamind@gmail.com",
       lattes: "http://lattes.cnpq.br/4733654913237092",
       imagem: ""  // <-- Só o nome do arquivo / se estiver sem foto no lattes adota foto default
 
