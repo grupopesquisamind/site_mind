88c75eb1571a0f1fdaac4bd63d990b52494ffb12
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2025-11-05 13:56:09 -0300

    Update dados-galeriacongressos.js

--- javascript/dados-galeriacongressos.js
+++ javascript/dados-galeriacongressos.js
@@ -6,7 +6,7 @@ function listarItens(tipo) {
       {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_3.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP/SP - Apresentação: Bianca Montemor e Ricardo Reis'},
       {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_4.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP/SP - Apresentação: Bianca Montemor e Ricardo Reis'},
       
-      {imagem: 'imagens/eventos/img/enebio_2024_banner_instituicao_evento_PUC_Minas_escola_feira_ciencias.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Belo Horizonte/MG - Instituição que promoveu o evento - PUC Minas e escola onde foi realizada a apresentaçã na feira ciências'},
+      {imagem: 'imagens/eventos/img/enebio_2024_banner_instituicao_evento_PUC_Minas_escola_feira_ciencias.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Belo Horizonte/MG - Instituição que promoveu o evento - PUC Minas e escola onde foi realizada a apresentação na feira ciências'},
       {imagem: 'imagens/eventos/img/enebio_2024_lucas_alexssandro_apresentacao.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Belo Horizonte/MG - Apresentação: Lucas Caraça e Alexssandro Ferreira'},
       {imagem: 'imagens/eventos/img/enebio_2024_lucas_apresentacao_2.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Belo Horizonte/MG - Apresentação: Lucas Caraça'},
       {imagem: 'imagens/eventos/img/enebio_2024_lucas_apresentacao_3.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Belo Horizonte/MG - Apresentação:  Lucas Caraça'},
@@ -14,7 +14,7 @@ function listarItens(tipo) {
 
       {imagem: 'imagens/eventos/img/eias_2024_imigracao.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Documentação Imigração'},
       {imagem: 'imagens/eventos/img/eias_2024_chegada_grupo_escola_evento_congresso.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai: chegada grupo no local do congresso'},
-      {imagem: 'imagens/eventos/img/eias_2024_alex_apresentacao_trabalho_gabriel_baroni.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentacao: Alexssandro Ferreira sobre o trabalho feito por Gabriel Baroni'},
+      {imagem: 'imagens/eventos/img/eias_2024_alex_apresentacao_trabalho_gabriel_baroni.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Alexssandro Ferreira sobre o trabalho feito por Gabriel Baroni'},
       {imagem: 'imagens/eventos/img/eias_2024_alex_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Alexssandro Ferreira'},
       {imagem: 'imagens/eventos/img/eias_2024_alex_paula_ivana.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Alexssandro, Ana Paula Kawabe e profa Ivana Elena (LTE-PECIM-Unicamp)'},
       {imagem: 'imagens/eventos/img/eias_2024_paula_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula Kawabe'},
