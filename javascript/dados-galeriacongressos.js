6a15d958f394d3751053f7906b510300ca1d685b
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2025-11-04 23:30:19 -0300

    Atualizar o dados-galeriacongressos.js

--- javascript/dados-galeriacongressos.js
+++ javascript/dados-galeriacongressos.js
@@ -6,15 +6,15 @@ function listarItens(tipo) {
       {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_3.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo'},
       {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_4.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo'},
       
-      {imagem: 'imagens/eventos/img/enebio_lucas_alexssandro_2024.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'SBENBIO/ENEBIO - Lucas e Alexssandro'},
+      {imagem: 'imagens/eventos/img/enebio_lucas_alexssandro_2024.jpg', sigla: 'ENEBIO', ano: '2024', descricao: 'Participação: Lucas e Alexssandro'},
 
       {imagem: 'imagens/eventos/img/EIAS_imigracao.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Documentação Imigração'},
       {imagem: 'imagens/eventos/img/EIAS_chegada_grupo_escola_evento_congresso.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai: chegada grupo no local do congresso'},
       {imagem: 'imagens/eventos/img/EIAS_alex_apresentacao_trabalho_gabriel_baroni.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentacao: Alexssandro sobre o trabalho feito por Gabriel Baroni'},
       {imagem: 'imagens/eventos/img/EIAS_alex_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Alexssandro'},
-      {imagem: 'imagens/eventos/img/EIAS_alex_paula_ivana.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Alexssandro, Ana Paula e profa Ivana Elena (LTE-Unicamp)'},
-      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula'},
-      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao_2.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula'},
+      {imagem: 'imagens/eventos/img/EIAS_alex_paula_ivana.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Alexssandro, Ana Paula Kawabe e profa Ivana Elena (LTE-Unicamp)'},
+      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula Kawabe'},
+      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao_2.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula Kawabe'},
       {imagem: 'imagens/eventos/img/EIAS_lucas_apresentacao.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Lucas'},
       {imagem: 'imagens/eventos/img/EIAS_lucas_apresentacao_2.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Lucas'},
       {imagem: 'imagens/eventos/img/EIAS_gabriel_kawabe_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Gabriel Kawabe'},
@@ -23,7 +23,7 @@ function listarItens(tipo) {
 
       {imagem: 'imagens/eventos/img/eppeq_2025_grupo.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Foto do Grupo'},
       {imagem: 'imagens/eventos/img/eppeq_2025.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação do Grupo'},
-      {imagem: 'imagens/eventos/img/eppeq_2025_grupo_2.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Thiago'},
+      {imagem: 'imagens/eventos/img/eppeq_2025_grupo_2.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Ana Paula Kawabe'},
       {imagem: 'imagens/eventos/img/eppeq_2025_A-C.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Ana Clara'},
       {imagem: 'imagens/eventos/img/eppeq_2025_y.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Yara'},
       {imagem: 'imagens/eventos/img/eppeq_2025_encerramento.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Cerimônia de Encerramento'},
