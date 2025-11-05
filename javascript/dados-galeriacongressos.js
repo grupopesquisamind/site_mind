3a1315d664e958cea7a39b016114acaea8c7c3dd
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2025-11-04 21:10:54 -0300

    ajuste e galeria de congressos

--- javascript/dados-galeriacongressos.js
+++ javascript/dados-galeriacongressos.js
@@ -1,12 +1,28 @@
 function listarItens(tipo) {
   if (tipo === 'galeriacongressos') {
     return [
-      {imagem: 'imagens/eventos/img/eppeq_2025_grupo.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Foto do grupo no EPPEQ 2025'},
-      {imagem: 'imagens/eventos/img/eppeq_2025.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Apresentação do Grupo EPPEQ 2025'},
-      {imagem: 'imagens/eventos/img/eppeq_2025_grupo_2.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Apresentação Thiago EPPEQ 2025'},
-      {imagem: 'imagens/eventos/img/eppeq_2025_A-C.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Apresentação de Ana Clara no EPPEQ 2025'},
-      {imagem: 'imagens/eventos/img/eppeq_2025_y.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Apresentação de Yara no EPPEQ 2025'},
-      {imagem: 'imagens/eventos/img/eppeq_2025_encerramento.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Encerramento do EPPEQ 2025'},
+      {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo no CONICT'},
+      {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_2.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo no CONICT'},
+      {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_3.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo no CONICT'},
+      {imagem: 'imagens/eventos/img/conict_2022_bianca_ricardo_apresentacao_4.jpeg', sigla: 'CONICT', ano: '2022', descricao: 'Reitoria IFSP - Apresentação: Bianca e Ricardo no CONICT'},
+      
+      {imagem: 'imagens/eventos/img/EIAS_imigracao.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Documentação Imigração'},
+      {imagem: 'imagens/eventos/img/EIAS_chegada_grupo_escola_evento_congresso.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai: chegada grupo no local do congresso'},
+      {imagem: 'imagens/eventos/img/EIAS_alex_apresentacao_trabalho_gabriel_baroni.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentacao: Alexssandro sobre o trabalho feito por Gabriel Baroni'},
+      {imagem: 'imagens/eventos/img/EIAS_alex_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Alexssandro'},
+      {imagem: 'imagens/eventos/img/EIAS_alex_paula_ivana.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Alexssandro, Ana Paula e profa Ivana Elena (LTE-Unicamp)'},
+      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula'},
+      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao_2.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula'},
+      {imagem: 'imagens/eventos/img/EIAS_paula_apresentacao_2.jpeg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Ana Paula'},
+      {imagem: 'imagens/eventos/img/EIAS_lucas_apresentacao.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Lucas'},
+      {imagem: 'imagens/eventos/img/EIAS_lucas_apresentacao_2.jpg', sigla: 'EIAS', ano: '2024', descricao: 'Montevideo Uruguai - Apresentação: Lucas'},
+
+      {imagem: 'imagens/eventos/img/eppeq_2025_grupo.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Foto do Grupo'},
+      {imagem: 'imagens/eventos/img/eppeq_2025.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação do Grupo'},
+      {imagem: 'imagens/eventos/img/eppeq_2025_grupo_2.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Thiago'},
+      {imagem: 'imagens/eventos/img/eppeq_2025_A-C.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Ana Clara'},
+      {imagem: 'imagens/eventos/img/eppeq_2025_y.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Apresentação: Yara'},
+      {imagem: 'imagens/eventos/img/eppeq_2025_encerramento.jpg', sigla: 'EPPEQ', ano: '2025', descricao: 'Presidente Prudente/SP - Cerimônia de Encerramento'},
     ];
   }
   return [];
