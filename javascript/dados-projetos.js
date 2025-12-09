47382dcb4bf6fa446e3eae67534f0f24fd7c960a
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2025-12-09 18:32:28 -0300

    pub e proj 2025

--- javascript/dados-projetos.js
+++ javascript/dados-projetos.js
@@ -329,29 +329,43 @@ function listarItens(chave) {
 {
     id: "26", /*enviado ao EPPEQ, aguardando publicação*/
     autor: "Yara Vitoria Claro de Freitas",
-    ano: "2024",
+    ano: "2025",
     nomeProducao: "Feromônios para o ensino de isomeria geométrica. (Aguardando publicação nos Anais do Congresso)",
     titulo: "Formigas, feromônios e Isômeros Geométricos: Proposta de Jogo Educacional Digital para a educação inclusiva de alunos com TEA.",
     categoria: "Química",
     tipo: "Jogo Educativo",
     src: "https://scratch.mit.edu/projects/1234855642/embed",
     linkPublicacao: "aviso.html",
-    publicacaoArquivo: "",
+    publicacaoArquivo: "aviso.html",
     referenciar: "FREITAS, Yara Vitoria Claro de; FERREIRA, Ana Paula Kawabe de Lima; SILVA, Alexssandro Ferreira da; NASCIMENTO, Ricardo Henrique dos Reis; CAMEJO AVILES, Ivana Elena. Formigas, feromônios e Isômeros Geométricos: Proposta de Jogo Educacional Digital para a educação inclusiva de alunos com TEA. 2025. In: EPPEQ-ENCONTRO PAULISTA DE PESQUISA EM ENSINO DE QUÍMICA, XIII, 2025, Presidente Prudente-SP. Anais [...]. Unesp: 2025, . Disponível em:",
   },
   {
     id: "27", /*enviado ao EPPEQ, aguardando publicação*/
     autor: "Thiago Marinho Santos",
-    ano: "2024",
+    ano: "2025",
     nomeProducao: "Máquina a vapor. (Aguardando publicação nos Anais do Congresso)",
     titulo: "Do Vapor à Inclusão: Um Recurso Didático Digital Baseado na História da Máquina a Vapor para Alunos com Transtorno do Espectro Autista.",
     categoria: "Física",
     tipo: "Jogo Educativo",
     src: "https://scratch.mit.edu/projects/1234857192/embed",
     linkPublicacao: "aviso.html",
-    publicacaoArquivo: "",
+    publicacaoArquivo: "aviso.html",
     referenciar: "SANTOS, Thiago Marinho; SILVA, Alexssandro Ferreira da; NASCIMENTO, Ricardo Henrique dos Reis; FERREIRA, Ana Paula Kawabe de Lima. Do Vapor à Inclusão: Um Recurso Didático Digital Baseado na História da Máquina a Vapor para Alunos com Transtorno do Espectro Autista. 2025. In: EPPEQ-ENCONTRO PAULISTA DE PESQUISA EM ENSINO DE QUÍMICA, XIII, 2025, Presidente Prudente-SP. Anais [...]. Unesp: 2025, . Disponível em:",
-  }
+  },
+  {
+    id: "28", /*enviado ao ENAS, aguardando publicação*/
+    autor: "Yara Vitória Claro de Freitas",
+    ano: "2025",
+    nomeProducao: "Sequência Didática sobre Isomeria Geométrica. (Aguardando publicação nos Anais do Congresso)",
+    titulo: "Aprendizagem Significativa Crítica e Inclusão: uma proposta de sequência didática sobre isomeria geométrica.",
+    categoria: "Química",
+    tipo: "Jogo Educativo",
+    src: "aviso.html",
+    linkPublicacao: "aviso.html",
+    publicacaoArquivo: "aviso.html",
+    referenciar: "FREITAS, Yara Vitoria Claro de; FERREIRA, Ana Paula Kawabe de Lima; SILVA, Alexssandro Ferreira da; CAMEJO AVILES, Ivana Elena. Aprendizagem Significativa Crítica e Inclusão: uma proposta de sequência didática sobre isomeria geométrica. In: Congresso 9º ENAS - Encontro Nacional de Aprendizagem Significativa, Univates de Lajeado-RS, 17-21/11/2025. Anais [...]. Disponível em: ",
+  },
+   
 ];
   }
   return [];
