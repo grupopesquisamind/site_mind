314e64a50469ddc3fb88dc8e6647d3e7f5dff2c3
MIND - Movimento para Inclusão e Neurodiversidade <grupopesquisamind@gmail.com>
2026-03-23 15:12:49 -0300

    Atualizar o footer.js

--- javascript/components/footer.js
+++ javascript/components/footer.js
@@ -2,7 +2,7 @@ class MeuFooter extends HTMLElement {
   async connectedCallback() {
 
     const ano = new Date().getFullYear();
-    const ultimaAtualizacao = '14/03/2026';
+    const ultimaAtualizacao = '23/03/2026';
     const versao = 'v1.2.0';
     const hoje = new Date().toISOString().split('T')[0];
     const caminhoAtual = window.location.pathname;
@@ -136,7 +136,7 @@ grupopesquisamind@gmail.com
 <p>
 Responsável:
 <span class="responsavel">
-Alexsandro Ferreira (Tec. Informação)
+Alexsandro Ferreira (Técnico de Tecnologia da Informação)
 </span>
 </p>
 
